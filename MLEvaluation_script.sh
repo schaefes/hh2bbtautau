4b0047f7d71fbeda51e4c5b605cc95ec9c6ac328
Stella S <stella.felice.schaefer@desy.de>
2024-03-22 12:42:59 +0100

    full update of the repository for full config (added version parameter)

--- MLEvaluation_script.sh
+++ MLEvaluation_script.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 10 --dataset graviton_hh_vbf_bbtautau_m400_madgraph --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset graviton_hh_ggf_bbtautau_m400_madgraph --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset tt_sl_powheg --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset tt_dl_powheg --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt50To100_amcatnlo --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt100To250_amcatnlo --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt250To400_amcatnlo --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt400To650_amcatnlo --calibrators skip_jecunc --remove-output 0,a,y
-law run cf.MLEvaluation --version PairsML --ml-model test --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt650_amcatnlo --calibrators skip_jecunc --remove-output 0,a,y
+while getopts m:w:v: flag
+do
+    case "${flag}" in
+        m) model=${OPTARG};;
+        w) worker=${OPTARG};;
+	v) ver=${OPTARG};;
+    esac
+done
+
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers $worker --dataset graviton_hh_vbf_bbtautau_m400_madgraph --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset graviton_hh_ggf_bbtautau_m400_madgraph --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset tt_sl_powheg --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset tt_dl_powheg --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt50To100_amcatnlo --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt100To250_amcatnlo --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt250To400_amcatnlo --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt400To650_amcatnlo --calibrators skip_jecunc
+law run cf.MLEvaluation --version $ver --ml-model $model --config run2_2017_nano_uhh_v11_limited --workers 1 --dataset dy_lep_pt650_amcatnlo --calibrators skip_jecunc
